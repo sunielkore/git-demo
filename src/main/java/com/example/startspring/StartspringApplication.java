a8df9464163878cf01b01dcbd96f664f10aec154
sunil_kore <sunielkore@gmail.com>
2019-12-20 21:36:08 +0530

    spring-boot first commit

---
+++ src/main/java/com/example/startspring/StartspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartspringApplication.class, args);
+	}
+
+}
