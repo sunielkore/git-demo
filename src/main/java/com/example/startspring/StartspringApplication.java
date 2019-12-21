08feb406b53bfd84bf692efc3b320acccad87fd0
sunil_kore <sunielkore@gmail.com>
2019-12-21 14:50:44 +0530

    first git remote commit

--- src/main/java/com/example/startspring/StartspringApplication.java
+++ src/main/java/com/example/startspring/StartspringApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCache
 public class StartspringApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(StartspringApplication.class, args);
 	}
 
